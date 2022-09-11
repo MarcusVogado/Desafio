7fbc153e8c85b57040b7a50cc74e2824a3d6ee4b
marcus vogado <marcus_620@hotmail.com>
2022-09-11 00:44:16 -0300

    update

--- Js/ScriptJava.js
+++ Js/ScriptJava.js
@@ -1,1 +1,24 @@
 // JavaScript source code
+
+// verificar se nomes e idade foram preenchidos usando java e verifica ser nome isNullOrEmpty
+
+
+function direcionarPage() {
+
+    const name = document.getElementById('userName').value;
+    const idade = document.getElementById('userIdade').value;
+
+    if (name == "" || idade==null) {
+        alert("Nome e Idade precisam ser preenchidos")
+    } else {
+        
+        window.location.href = "PageSorteio.html"
+    }        
+                
+}
+function sortearNumeros() {
+
+  const sorteado = Math.floor(( Math.random()* 5)+1);
+
+    document.getElementById('result').innerHTML = sorteado;
+}
