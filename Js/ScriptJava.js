6237e048db66d663716665ec2c1d4321877e7025
marcus vogado <marcus_620@hotmail.com>
2022-09-12 18:17:49 -0300

    update

--- Js/ScriptJava.js
+++ Js/ScriptJava.js
@@ -1,24 +1,39 @@
 // JavaScript source code
 
-// verificar se nomes e idade foram preenchidos usando java e verifica ser nome isNullOrEmpty
+
+if (localStorage.info) {    
+    document.getElementById('userName').innerHTML =localStorage.info;
+}
 
 
 function direcionarPage() {
 
     const name = document.getElementById('userName').value;
+    localStorage.info = document.getElementById('userName').value;
     const idade = document.getElementById('userIdade').value;
-
-    if (name == "" || idade==null) {
+    
+    if (name == "" || idade == "") {
         alert("Nome e Idade precisam ser preenchidos")
-    } else {
-        
+    }
+    else {
+               
         window.location.href = "PageSorteio.html"
-    }        
                 
+    }
+
 }
 function sortearNumeros() {
 
-  const sorteado = Math.floor(( Math.random()* 5)+1);
-
+    const sorteado = Math.floor((Math.random() * 5) + 1);
     document.getElementById('result').innerHTML = sorteado;
-}
+    document.getElementById("listaSorteio").innerHTML = "";
+
+    for (let i = 1; i <= sorteado; i++) {
+        let item = document.createElement('li');
+        item.appendChild(document.createTextNode("**** SORTEIO ****"));
+        listaSorteio.appendChild(item);
+
+
+    }
+    
+} 
